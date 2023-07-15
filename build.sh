fa06f1964414cb7c99fe8d584d2c097e9f2bfc56
DanielDDsam <salaodxgotico91@gmail.com>
2023-07-15 15:34:57 -0400

    dasd

--- build.sh
+++ build.sh
@@ -6,5 +6,4 @@ pip install -r requirements.txt
 
 python manage.py collectstatic --no-input
 python manage.py migrate
-python manage.py shell < Scripts/reset_db.py
 python manage.py shell < Scripts/main.py
